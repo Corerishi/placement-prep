af26b37041c88014af515fc8f94801b0bcc4a5cc
Corerishi <rishiraj.xd@gmail.com>
2026-08-04 08:59:16 +0530

    Day01: LogicBuilding101 - problem1.py

---
+++ logicbuilding101/day01/problem1.py
@@ -0,0 +1,11 @@
+"""
+Problem: print integer enterd by the user
+Input: user enters the the number
+Output: prints the same number
+
+Approach:
+read the user input using input and output the same number as result
+
+"""
+number = int(input("Enter the number: "))
+print("The number is: ", number)
